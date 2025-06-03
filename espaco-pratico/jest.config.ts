0e8eda46ccf2e04190d035f6298a27ba03c957a6
Catarina C Sampaio <catarinacs@ciandt.com>
2025-06-03 15:46:11 -0300

    feat: configurações para teste com jest

---
+++ espaco-pratico/jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+};
+
+export default config;
