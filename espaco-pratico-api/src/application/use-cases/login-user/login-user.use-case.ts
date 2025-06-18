5dcdd283ef33509d51fb955c8aa06a4af9f9e211
lucasi <lucasi@ciandt.com>
2025-06-18 11:40:33 -0300

    fix: remove unused User entity import from login-user use case

--- espaco-pratico-api/src/application/use-cases/login-user/login-user.use-case.ts
+++ espaco-pratico-api/src/application/use-cases/login-user/login-user.use-case.ts
@@ -1,6 +1,5 @@
 import { ILoginUserDTO } from "../../dto/login-user.dto";
 import { IUserRepository } from "../../../domain/interfaces/repositories/user-repository.interface";
-import { User } from "../../../domain/entities/userEntity/user.entity";
 
 export class LoginUserUseCase {
     constructor(private readonly userRepository: IUserRepository) {}
