e901a41d6ce8c1e620735c61c6e08738dcd574e7
lucasi <lucasi@ciandt.com>
2025-06-18 13:33:21 -0300

    feat: add user authentication with login and register functionality

--- espaco-pratico-api/src/interfaces/http/http.module.ts
+++ espaco-pratico-api/src/interfaces/http/http.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
+import { UserController } from './controllers/user.controller';
+import { LoginUserUseCase } from '../../application/use-cases/login-user/login-user.use-case';
+import { RegisterUserUseCase } from '../../application/use-cases/register-user/register-user.use-case';
+import { UserRepository } from '../../infrastructure/database/mongo/repositories/user-repository/user.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { userSchema } from '../../infrastructure/database/mongo/schemas/user.schema';
 
 @Module({
-  controllers: [AppController],
+  controllers: [AppController, UserController],
+  providers: [LoginUserUseCase, RegisterUserUseCase, UserRepository],
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: userSchema }])],
 })
 export class HttpModule {}
